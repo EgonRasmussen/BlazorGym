3c09fbbf43a6b390f047b603aad95c012676e87e
EgonRasmussen <ecr@eucsyd.dk>
2025-12-24 10:39:17 +0100

    ok

---
+++ BlazorGym/Components/Models.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorGym.Components;
+
+public static class Countries
+{
+    public static Dictionary<string, string> All { get; } = new()
+    {
+        ["us"] = "United States",
+        ["ca"] = "Canada",
+        ["uk"] = "United Kingdom",
+        ["be"] = "Belgium"
+    };
+}
+public enum Gender
+{
+    Female, Male
+}
+public class Member
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public required string Country { get; set; }
+    public bool Subscriber { get; set; }
+    public Gender Gender { get; set; }
+}
